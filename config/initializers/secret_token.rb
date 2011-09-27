244567d6f19d62e04e378edc3739162cc1248f02
Alex Lewis <ebcomalex@gmail.com>
2011-09-27 10:34:30 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Columbusgacalendar::Application.config.secret_token = 'e0b6348d96f2d84eaa2e823214e869dc7c215ffe87804dfb4ac69c0c2e98a4bdbd69da2087bec2d9286df2ab8beb6179ab5c4ec21e9b95c1f994fd5bf72f0a1f'
